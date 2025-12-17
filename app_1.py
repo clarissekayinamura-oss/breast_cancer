9adbb82d74022a8617b77124a22651d1d65e7209
clarissekayinamura-oss <clarissekayinamura@gmail.com>
2025-12-17 13:57:31 +0300

    Add files via upload

---
+++ app_1.py
@@ -0,0 +1,34 @@
+
+
+import streamlit as st
+import pickle
+
+# Load trained model
+with open('SVM_model_GROUP_6.pkl', 'rb') as file:
+    model = pickle.load(file)
+
+# App title
+st.title("BREAST CANCER ")
+st.title("25RP21212 ")
+
+# User input
+st.write("Enter patient measurements:")
+
+radius_mean = st.number_input("Radius Mean")
+texture_mean = st.number_input("Texture Mean")
+perimeter_mean = st.number_input("Perimeter Mean")
+area_mean = st.number_input("Area Mean")
+
+# Prediction button
+if st.button("Predict"):
+    input_data = [[radius_mean, texture_mean, perimeter_mean, area_mean]]
+    prediction = model.predict(input_data)
+    
+    if prediction[0] == 1:
+        st.error("Prediction: Malignant")
+    else:
+        st.success("Prediction: Benign")
+
+#cd C:\Users\U\Desktop\breastcancer
+#dir to list
+
